4e8eb18e619de7d32b13daabcb9ee81949160e8c
Benedikt Döring <benediktdoering3@gmail.com>
2025-06-20 16:30:50 +0200

    fix(views.py): Use correct autonomous

--- core/views.py
+++ core/views.py
@@ -6,23 +6,21 @@ import json
 from django.shortcuts import render, redirect
 from .interface import move_robot_safecast_linear
 from . import core_main
-autonomous = False
+
 def steuerung(request):
-   global autonomous
    message = ""
    if request.method == 'POST':
        if 'direction' in request.POST:
            direction = request.POST['direction']
-           if not autonomous:
-               message = f"Bewegung: {direction}"
+           if not core_main.our_status.get_is_autonomous():
+               move_robot_safecast_linear(direction) #TODO call from other thread?
+               core_main.our_status.set_message(f"Bewegung: {direction}")
            else:
-               message = "Autonomer Modus aktiviert. Manuelle Steuerung deaktiviert."
+               core_main.our_status.set_message("Autonomer Modus aktiviert. Manuelle Steuerung deaktiviert.")
        if 'autonomous' in request.POST:
-           autonomous = not autonomous
-           message = "Modus geändert"
-           core_main.our_status.set_is_autonomous(True if autonomous=="on" else False)
-
-   status =  core_main.get_system_status()#todo move dict conversion into get_status / remove double keys.
+           core_main.our_status.set_is_autonomous(not core_main.our_status.get_is_autonomous())
+           core_main.our_status.set_message("Modus geändert")
+   status = core_main.get_system_status()  #todo move dict conversion into get_status / remove double keys.
     
    return render(request, 'steuerung.html',json.loads(status.to_json()))
 
